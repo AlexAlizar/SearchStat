9124967c4dfd55ad37b99aae6e7f5d2346cd73c7
AlexRustler <alex_rustler@rambler.ru>
2017-12-19 00:05:58 +0700

    Корректная обработка в случае указания адреса без протокола

--- krauler/crawlerGeekBrains/src/ru/geekbrains/krawler/Downloader.java
+++ krauler/crawlerGeekBrains/src/ru/geekbrains/krawler/Downloader.java
@@ -1,16 +1,39 @@
 package ru.geekbrains.krawler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.net.URL;
 
 /**
  * Created by User on 16.12.2017.
  */
+
+
 public class Downloader {
+    final static String DEFAULT_PROTOCOL = "https://";
+    final static String ALTERNATIVE_PROTOCOL = "http://";
 
     public static String download(String url) {
+
+        String content, defaultProtocol, alternativeProtocol;
+
+        url = url.toLowerCase();
+        defaultProtocol = checkProtocolURL(url);
+        alternativeProtocol = getAlternativeProtocol(defaultProtocol);
+        content = getContent(defaultProtocol + url);
+        if (!checkContent(content)){
+            content = getContent(alternativeProtocol + url);
+            if (!checkContent(content)) {
+                return "Указан некорректный веб-адрес";
+            }
+        }
+        return content;
+    }
+
+
+    public static String getContent(String url) {
+
         BufferedReader reader = null;
         StringBuilder result = new StringBuilder();
         try {
@@ -30,5 +53,27 @@ public class Downloader {
             }
         }
         return result.toString();
+    }
+
+    private static String checkProtocolURL(String url) {
+
+        int index = url.indexOf("://");
+        if (index < 0) {
+            return DEFAULT_PROTOCOL;
+        } else {
+            return url.substring(0,index+3);
+        }
+    }
+
+    private static String getAlternativeProtocol(String Protocol) {
+        if (Protocol.equals(DEFAULT_PROTOCOL)) {
+            return ALTERNATIVE_PROTOCOL;
+        } else {
+            return DEFAULT_PROTOCOL;
+        }
+    }
+
+    private static boolean checkContent(String Content) {
+        return true;
     }
 }

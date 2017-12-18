120c2c855f008156d454f4e2065c11d7fb690c41
Evgeny <skilioty@inbox.ru>
2017-12-18 14:53:16 +0300

    Create project

---
+++ ios_ui/SearchStat/SearchStat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchStat
+//
+//  Created by Евгений Скилиоти on 18.12.2017.
+//  Copyright © 2017 Евгений Скилиоти. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

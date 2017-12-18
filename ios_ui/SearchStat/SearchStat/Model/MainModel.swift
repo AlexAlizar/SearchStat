bd09dc705acbb07839f8aab99980393706fce600
Roman Trekhlebov <xaero_ol@mail.ru>
2017-12-18 15:13:44 +0300

    Model Init

---
+++ ios_ui/SearchStat/SearchStat/Model/MainModel.swift
@@ -0,0 +1,23 @@
+//
+//  MainModel.swift
+//  SearchStat
+//
+//  Created by Roman Trekhlebov on 18.12.2017.
+//  Copyright © 2017 Евгений Скилиоти. All rights reserved.
+//
+
+import Foundation
+//Модель:
+//1. Site
+//id : Int
+//name: String
+//personsArray: [Person]
+//2. Person
+//id: Int
+//name: String
+//total: Int
+//dayStatsArray: [DayStats]
+//3. DayStats
+//day: DateTime
+//total: Int
+

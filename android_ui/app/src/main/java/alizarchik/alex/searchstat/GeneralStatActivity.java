36697fc91a0f6b9b6ff03af987bbd371b7d2d2c4
aa <email>
2017-12-20 12:50:40 +0300

    ок

---
+++ android_ui/app/src/main/java/alizarchik/alex/searchstat/GeneralStatActivity.java
@@ -0,0 +1,13 @@
+package alizarchik.alex.searchstat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeneralStatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_stat);
+    }
+}

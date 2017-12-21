55a39b8dc9b094da361a88cebe22175a51f457c0
Nick A <fmpanic@gmail.com>
2017-12-21 16:44:08 +0300

    create base routing, add base template \ все фигня Михалыч, давай по-новой

--- web_ai/web_ai/admin_interface/urls.py
+++ web_ai/web_ai/admin_interface/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import admin_interface, sites_view, add_site, delete_site
+from .admin_sites_views import admin_interface, sites_view, add_site, delete_site
 urlpatterns = [
     path('', admin_interface, name='admin_interface'),
+    # sites_manage_views
     path('sites', sites_view, name='sites_view'),
     path('sites/add/', add_site, name='add_site'),
     path('sites/delete', delete_site, name='delete_site'),
+
+    # persons_manage_views
+
+    # keywords_manage_views
+
 ]

4f61fb09975eb4b84d5c69c66dc62f87e2277e3e
Alloyer <alexeygruntov@yandex.ru>
2017-12-22 22:19:07 +0300

    добавлены тесты для проверки работоспособности методов работы с БД

--- krauler/crawlerGeekBrains/src/main/java/ru/geekbrains/krawler/Main.java
+++ krauler/crawlerGeekBrains/src/main/java/ru/geekbrains/krawler/Main.java
@@ -12,10 +12,27 @@ public class Main {
 
         System.out.println("Проверка от Алексей Грунтов =)");
 
+        /**
+         * чтобы всё работало, надо:
+         * 1.1) создать connection в MySQL
+         * 1.2) проврить чтобы пароль от созданного connection совпадал с тем под которым вы входите туда
+         * 2) создать schema "test"
+         * 3) если программа запускается в первый раз, то надо поменять параметр hbm2ddl.auto на create; в последующие запуски надо чтобы этот параметр был равен update
+         * 4) ВУАЛЯ - можно использовать методы add/insert Person
+         */
+
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
         DBService dbService = new DBService(sessionFactory);
-        dbService.addPerson("Vasya");
+        //НЕ ДОБАВЛЯЙТЕ ОДИНАКОВЫХ ПОЛЬЗОВАТЕЛЕЙ, Т.К. МЕТОД getPersonByName пока что ругается на это; надо сделать имена персон уникальными в БД, тогда всё будет ок
+        //dbService.addPerson("Petya");
+
+        try {
+            //System.out.println(dbService.getPersonById(2).toString());
+            System.out.println(dbService.getPersonByName("Petya").toString());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 
     }
 }

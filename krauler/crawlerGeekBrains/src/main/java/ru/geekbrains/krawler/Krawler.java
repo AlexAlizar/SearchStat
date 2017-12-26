85e394328c5425d46918c55681f15f175aecaeed
sparrow84 <vorobey280184@mail.ru>
2017-12-26 22:57:00 +0700

    Работаю над Krawler

--- krauler/crawlerGeekBrains/src/main/java/ru/geekbrains/krawler/Krawler.java
+++ krauler/crawlerGeekBrains/src/main/java/ru/geekbrains/krawler/Krawler.java
@@ -1,5 +1,8 @@
 package ru.geekbrains.krawler;
 
+import dbService.DBService;
+import dbService.dataSets.Person;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import java.util.List;
@@ -7,9 +10,19 @@ import java.util.List;
 public class Krawler {
     public static void kStart () {
 
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        DBService dbService = new DBService(sessionFactory);
+
         searchPageLinks();
 
         List<String> targetsForStatistics = null; // получить из базы список целей по которым нужна статистика
+
+        dbService.
+
+        Person person = dbService.getPersonByName("Путин");
+        keywords = dbService.getKeywordByPerson(person);
+
 
         List<String> sites = null; // получить из базы список сайтов
 
@@ -25,18 +38,24 @@ public class Krawler {
 
         /*ПОИСК ДЛЯ ОДНОГО ЧЕЛОВЕКА*/
 
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        DBService dbService = new DBService(sessionFactory);
+
         List<String> pageLinks = null;          /*получить из базы список ссылок на страницы для конкретного сайта*/
 
         List<String> keyWords = null;           /*запросить из базы список искомых слов*/
+
+        keyWords = dbService.getKeywordByPerson(target);
 
         int count = 0;                          // количество найденных совпадений
 
 
         for (String pageLink : pageLinks) {     // бежим по страницам
 
-            for (String key : keyWords) {   // бежим по искомым словам
+            for (String key : keyWords) {       // бежим по искомым словам
 
-                count += PageParser.parsePage(Downloader.download(pageLink),key);
+                count += PageParser.parsePage(Downloader.download(pageLink),key); // Скачиваем страницу, ищем ключевые слова и записываем количесво найденных совпадений
 
             }
 
@@ -50,7 +69,7 @@ public class Krawler {
 
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
-//        sessionFactory.
+        DBService dbService = new DBService(sessionFactory);
 
         List<String> sitesList = null;       /*получить из базы список ссылок на сайты*/
 

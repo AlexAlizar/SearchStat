fc49e9570c221479db5b9408bca84074a7d34a92
aa <email>
2017-12-27 01:27:11 +0300

    Merge branch 'android_ui' of https://github.com/AlexAlizar/SearchStat into android_ui

--- android_ui/app/src/main/java/alizarchik/alex/searchstat/Model/Site.java
+++ android_ui/app/src/main/java/alizarchik/alex/searchstat/Model/Site.java
@@ -1,22 +1,46 @@
 package alizarchik.alex.searchstat.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
 /**
  * Created by aoalizarchik.
  */
 
 public class Site {
+    @SerializedName("SiteName")
+    @Expose
+    private String siteName;
+    @SerializedName("persons")
+    @Expose
+    private ArrayList<Person> persons;
 
-    String name;
-
-    public Site(String name) {
-        this.name = name;
+    public Site(String siteName) {
+        this.siteName = siteName;
     }
 
-    public String getName() {
-        return name;
+    public String getSiteName() {
+        return siteName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+    @Override
+    public String toString() {
+        return "Site{" +
+                "siteName='" + siteName + '\'' +
+                '}';
     }
 }

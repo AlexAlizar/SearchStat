7c17412e844126a3f94a0731167960ccece210f9
TimKaaz <Timofeykazaev@gmail.com>
2017-12-26 20:15:49 -0700

    Later and Previously buttons connected. calendarButton connected.

--- ios_ui/SearchStat/SearchStat/Controller/DetailStatisticViewController.swift
+++ ios_ui/SearchStat/SearchStat/Controller/DetailStatisticViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class DetailStatisticViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class DetailStatisticViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, FSCalendarDelegate, FSCalendarDataSource {
 
+    
+    //MARK: Variables for calendar
+    // к след релизу перенесем на отдельный VC
+    
+    var unixDay = TimeInterval(86400)
+    var currentDate = Date()
+    
+    fileprivate let gregorian = Calendar(identifier: .gregorian)
+    fileprivate let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM yyyy"
+        return formatter
+    }()
+    
+    fileprivate weak var calendar: FSCalendar!
+    
+    @IBOutlet weak var detailCalendarConstraint: NSLayoutConstraint!
+    
+    @IBAction func previouslyButton(_ sender: UIButton) {
+        currentDate -= unixDay
+        calendarButton.setTitle(self.formatter.string(from: currentDate), for: .normal)
+    }
+    
+    @IBAction func laterButton(_ sender: UIButton) {
+        currentDate += unixDay
+        calendarButton.setTitle(self.formatter.string(from: currentDate), for: .normal)
+    }
+    
+    //=========================
+    
     
     @IBOutlet weak var detailTableView: UITableView!
     @IBOutlet weak var calendarButton: UIButton!
@@ -21,31 +51,23 @@ class DetailStatisticViewController: UIViewController, UITableViewDelegate, UITa
     @IBOutlet weak var nameSourceLabel: UILabel!
     @IBOutlet weak var detailCalendarView: UIView!
     @IBOutlet weak var detailBackGroundBtn: UIButton!
-    
-  
-    @IBOutlet weak var detailCalendarConstraint: NSLayoutConstraint!
-   
     @IBAction func calendarDetailTapped(_ sender: UIButton) {
-        detailCalendarConstraint.constant = 0
-        detailBackGroundBtn.alpha = 0.5
-        UIView.animate(withDuration: 0.3) {
+        
+        UIView.animate(withDuration: 0.2) {
+            self.detailCalendarConstraint.constant = -80
+            self.detailBackGroundBtn.alpha = 0.5
             self.view.layoutIfNeeded()
         }
     }
     
     @IBAction func hideDetailCalendar(_ sender: UIButton) {
-        detailCalendarConstraint.constant = 375
-        detailBackGroundBtn.alpha = 0
-        UIView.animate(withDuration: 0.1) {
-            self.view.layoutIfNeeded()
-        }
+        dissMissCalendar()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       nameSourceLabel.text = nameSourceLabelStr
-        calendarButton.setTitle(MainService.instance.lastUpdateDate, for: .normal)
+        nameSourceLabel.text = nameSourceLabelStr
         
         MainService.instance.getPerson { (result) in
             if result {
@@ -59,6 +81,26 @@ class DetailStatisticViewController: UIViewController, UITableViewDelegate, UITa
         }
     }
     
+    
+    // MARK: Это тоже перенесем
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date , at monthPosition: FSCalendarMonthPosition) {
+
+        currentDate = date
+        calendarButton.setTitle(self.formatter.string(from: date), for: .normal)
+        dissMissCalendar()
+
+    }
+    
+    func dissMissCalendar() {
+        UIView.animate(withDuration: 0.1) {
+            self.detailCalendarConstraint.constant = 600
+            self.detailBackGroundBtn.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //=========================
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return personArray.count

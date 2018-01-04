da6a3942eecf811673f1cf5efa6cdcb620e8c8c0
Nick A <fmpanic@gmail.com>
2018-01-04 16:55:22 +0300

    подключена удаленная тестовая база данных

--- web_ai/web_ai/web_ai/settings.py
+++ web_ai/web_ai/web_ai/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'web_ai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '195.110.59.16',
+        'PORT': '3306',
+        'NAME': 'searchstat_django_testdrive',
+        'USER': 'django_unchained',
+        'PASSWORD': 'testpass',
+        'OPTIONS': {
+            # 'read_default_file': '/etc/mysql/my.cnf',
+            'init_command': 'SET default_storage_engine=INNODB',
+        }
     }
 }
 

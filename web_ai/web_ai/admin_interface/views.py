dc75ff7dbb33f08e85f702033c0991436d6083e4
Nick A <fmpanic@gmail.com>
2018-01-08 23:02:48 +0300

    статистика краулера. список страниц

--- web_ai/web_ai/admin_interface/views.py
+++ web_ai/web_ai/admin_interface/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_list_or_404, render_to_response
 from .models import Sites, Pages
+from collections import defaultdict
 
 
 def admin_interface(request):
@@ -9,7 +10,17 @@ def admin_interface(request):
 
 def statistic(request):
     sites = get_list_or_404(Sites.objects.all())
-    pages = get_list_or_404(Pages.objects.all())
+
+    x = Pages.objects.values_list('site__name', 'url')
+    pages_dict = {}
+    for i in x:
+        if i[0] in pages_dict:
+            pages_dict[i[0]].append(i[1])
+        else:
+            pages_dict[i[0]] = [i[1]]
+
     total_sites = len(sites)
-    total_pages = len(pages)
-    return render_to_response('statistic.html', {'total_sites': total_sites, 'total_pages': total_pages})
+    total_pages = len(x)
+    return render_to_response('statistic.html', {'total_sites': total_sites,
+                                                 'total_pages': total_pages,
+                                                 'pages_dict': pages_dict})

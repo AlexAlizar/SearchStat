0023de5dcaa4051c8e12973ac58852216d02dc35
aa <email>
2018-01-09 19:20:23 +0300

    не обращай внимание

---
+++ android_ui/app/src/main/java/alizarchik/alex/searchstat/Model/DailyStatisticsModel.java
@@ -0,0 +1,33 @@
+package alizarchik.alex.searchstat.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Olesia on 25.12.2017.
+ */
+
+public class DailyStatisticsModel {
+    @SerializedName("person")
+    @Expose
+    private String person;
+    @SerializedName("page")
+    @Expose
+    private int page;
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}

a55c949ea5b03b61b642ccf525bbf0be701d8fbb
Roman Trekhlebov <xaero_ol@mail.ru>
2018-01-10 11:23:19 +0300

    Model FIX

--- ios_ui/SearchStat/SearchStat/Model/MainModel.swift
+++ ios_ui/SearchStat/SearchStat/Model/MainModel.swift
@@ -68,8 +68,8 @@ struct Person {
     
     func isDatesEqual(dateOne: Date, dateTwo: Date) -> Bool {
     
-        let unixDateOne = dateOne.timeIntervalSince1970
-        let unixDateTwo = dateTwo.timeIntervalSince1970
+        let unixDateOne = Int(dateOne.timeIntervalSince1970 / 86400)
+        let unixDateTwo = Int(dateTwo.timeIntervalSince1970 / 86400)
        
         return unixDateOne == unixDateTwo ? true : false
     }

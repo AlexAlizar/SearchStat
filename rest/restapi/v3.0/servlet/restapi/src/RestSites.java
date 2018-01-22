f389f28e57130b34704a5712f57d4ca242a24a49
tbpxxi <tbpxxi@yandex.ru>
2018-01-23 01:56:35 +0300

    v3.0 Added actions for the users: "get-users", "add-user", "delete-user"

---
+++ rest/restapi/v3.0/servlet/restapi/src/RestSites.java
@@ -0,0 +1,9 @@
+public class RestSites {
+    private String id;
+    private String name;
+
+    public RestSites(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}

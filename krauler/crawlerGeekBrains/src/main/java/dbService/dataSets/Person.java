c7d548ac288b3257813596971eba6680788cb19e
AlexRustler <alex_rustler@rambler.ru>
2018-01-28 10:38:48 +0700

    Добавил методы для сравнения объектов класса Person

--- krauler/crawlerGeekBrains/src/main/java/dbService/dataSets/Person.java
+++ krauler/crawlerGeekBrains/src/main/java/dbService/dataSets/Person.java
@@ -49,4 +49,22 @@ public class Person {
                 ", name='" + name + '\'' +
                 "}";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
 }
